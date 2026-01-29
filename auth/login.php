4e0ac598f530fa6dd84c1fbd7fc007ee707f41c4
Deep karmakar <159227454+deepkarmakar-dev@users.noreply.github.com>
2026-01-29 13:59:58 +0530

    Redirect login to index.php instead of dashboard.php

--- auth/login.php
+++ auth/login.php
@@ -78,7 +78,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         $_SESSION['user_id']   = $user['id'];
                         $_SESSION['user_name'] = $user['name'];
 
-                        header("Location: ../dashboard.php");
+                        header("Location: ../index.php");
                         exit;
 
                     } else {
